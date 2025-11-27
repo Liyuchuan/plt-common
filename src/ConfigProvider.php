94a8131b3a16030a1c4eaa060b7aa18ac3114c04
liyuchuan <908128659@qq.com>
2025-11-27 20:31:51 +0800

    ConfigProvider

---
+++ src/ConfigProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Liyuchuan\PltCommon;
+
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'annotations' => [
+                'scan' => [
+                    'paths' => [
+                        __DIR__,
+                    ],
+                ],
+            ],
+        ];
+    }
+}
